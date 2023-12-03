df8dda958496486561e7d9eab3d263245278cd8a
CodingKodama <Coding@Kodama>
2023-12-03 20:22:36 +0100

    Day 01 Lacking regex-fu so doing some reverse shenanigans

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,59 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map {
+            val firstDigit = "^\\D*(\\d)".toRegex().find(it)?.groupValues?.get(1)
+            val secondDigit = "(\\d)\\D*$".toRegex().find(it)?.groupValues?.get(1)
+            firstDigit + secondDigit
+        }.sumOf { it.toInt() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun wash(input: String): String {
+        return input.replace("one|two|three|four|five|six|seven|eight|nine".toRegex()) {
+            when (it.value) {
+                "one" -> "1"
+                "two" -> "2"
+                "three" -> "3"
+                "four" -> "4"
+                "five" -> "5"
+                "six" -> "6"
+                "seven" -> "7"
+                "eight" -> "8"
+                "nine" -> "9"
+                else -> it.value
+            }
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun rewash(input: String): String {
+        return input.reversed().replace("one|two|three|four|five|six|seven|eight|nine".reversed().toRegex()) {
+            when (it.value) {
+                "one".reversed() -> "1"
+                "two".reversed() -> "2"
+                "three".reversed() -> "3"
+                "four".reversed() -> "4"
+                "five".reversed() -> "5"
+                "six".reversed() -> "6"
+                "seven".reversed() -> "7"
+                "eight".reversed() -> "8"
+                "nine".reversed() -> "9"
+                else -> it.value
+            }
+        }
+    }
+
+    fun part2UglyReverseHack(input: List<String>): Int {
+        return input.map {
+            val firstDigit = "^\\D*(\\d)".toRegex().find(wash(it))?.groupValues?.get(1)
+            val secondDigit = "^\\D*(\\d)".toRegex().find(rewash(it))?.groupValues?.get(1)
+            firstDigit + secondDigit
+        }.sumOf { it.toInt() }
+    }
+
+
+    check(part1(readInput("Day01_test")) == 142)
+    check(part2UglyReverseHack(readInput("Day01_test_part2")) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+    part2UglyReverseHack(input).println()
 }
